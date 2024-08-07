230cd54d74f3bf5257d95d0e92af6ef37d7ec375
ybw0014 <i@guizhanss.com>
2024-08-07 11:30:12 -0400

    fix(StackUtils): 修复 1.20.5 以上 PotionMeta 比较

--- src/main/java/io/github/sefiraat/networks/utils/StackUtils.java
+++ src/main/java/io/github/sefiraat/networks/utils/StackUtils.java
@@ -1,6 +1,7 @@
 package io.github.sefiraat.networks.utils;
 
 import io.github.sefiraat.networks.network.stackcaches.ItemStackCache;
+import io.github.thebusybiscuit.slimefun4.api.MinecraftVersion;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.data.persistent.PersistentDataAPI;
 import lombok.experimental.UtilityClass;
@@ -268,8 +269,14 @@ public class StackUtils {
 
         // Potion
         if (metaOne instanceof PotionMeta instanceOne && metaTwo instanceof PotionMeta instanceTwo) {
-            if (!instanceOne.getBasePotionData().equals(instanceTwo.getBasePotionData())) {
-                return true;
+            if (Slimefun.getMinecraftVersion().isAtLeast(MinecraftVersion.MINECRAFT_1_20_5)) {
+                if (instanceOne.getBasePotionType() != instanceTwo.getBasePotionType()) {
+                    return true;
+                }
+            } else {
+                if (!instanceOne.getBasePotionData().equals(instanceTwo.getBasePotionData())) {
+                    return true;
+                }
             }
             if (instanceOne.hasCustomEffects() != instanceTwo.hasCustomEffects()) {
                 return true;
