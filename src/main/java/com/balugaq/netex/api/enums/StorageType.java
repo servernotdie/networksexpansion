301a8e5581c28d830761629f25fea79ea1302290
balugaq <129668183+balugaq@users.noreply.github.com>
2025-01-31 22:43:44 +0800

    fix

--- src/main/java/com/balugaq/netex/api/enums/StorageType.java
+++ src/main/java/com/balugaq/netex/api/enums/StorageType.java
@@ -1,6 +1,6 @@
 package com.balugaq.netex.api.enums;
 
-public interface StorageType {
-    StorageType BARREL = new StorageType() {};
-    StorageType DRAWER = new StorageType() {};
+public enum StorageType {
+    BARREL,
+    DRAWER;
 }
